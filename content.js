2e476a0fba6117999bfad3db2a4c7056f4b81828
Can Gür <49841094+cangurdev@users.noreply.github.com>
2024-09-02 00:53:00 +0300

    add last raid amounts

--- content.js
+++ content.js
@@ -18,7 +18,12 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                       id    = "find"
                       class = "layoutButton buttonFramed round market"
                       style = "padding:10px; color:#fff; margin-bottom 20px;"
-                    >Ara</button>
+                    >Köyler</button>
+                    <button 
+                      id    = "raid"
+                      class = "layoutButton buttonFramed round market"
+                      style = "padding:10px; color:#fff; margin-bottom 20px;"
+                    >Yağma</button>
                     <div id='table' style="max-height: 300px; overflow:auto; margin-top:20px"></div>
                     </div>
                 </div>
@@ -31,6 +36,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
     document.getElementById('find').addEventListener('click', () => {
       findVillage();
+    });
+
+    document.getElementById('raid').addEventListener('click', () => {
+      getRaidBounties();
     });
   }
 });
@@ -48,11 +57,7 @@ async function findVillage() {
   sendReq().then(res => {
     const tiles = res.tiles;
 
-    const table = document.createElement("table");
-    const thead = document.createElement("thead");
-    const tbody = document.createElement("tbody");
-
-    thead.innerHTML = `
+    const theadInnerHtml = `
         <tr>
           <th>Köy</th>
           <th>Halk</th>
@@ -72,13 +77,13 @@ async function findVillage() {
         const distance = getDistance(myX, myY, tile.position.x, tile.position.y);
 
         const model = {
-          name: attrs.name,
-          tribe: attrs.tribe,
+          name      : attrs.name,
+          tribe     : attrs.tribe,
           population: attrs.population,
-          clan: attrs.clan,
-          distance: distance,
-          x: tile.position.x,
-          y: tile.position.y
+          clan      : attrs.clan,
+          distance  : distance,
+          x         : tile.position.x,
+          y         : tile.position.y
         }
 
         if (attrs.population?.includes("Fil")) {
@@ -94,11 +99,10 @@ async function findVillage() {
     });
 
     const list = elephantList.concat(tileList);
+    const rowInnerHtml = [];
 
     list.forEach((tile) => {
-      const row = document.createElement("tr");
-
-      row.innerHTML = `
+      rowInnerHtml.push(`
           <td><a 
             target="_blank" 
             href="https://ts8.x1.europe.travian.com/karte.php?x=${tile.x}&y=${tile.y}"
@@ -110,17 +114,10 @@ async function findVillage() {
           <td>${tile.population}</td>
           <td>${tile.clan || ""}</td>
           <td>${tile.distance.toFixed(1)}<br>${getTime(tile.distance)}</td>
-        `;
-
-      tbody.appendChild(row);
+        `)
     });
 
-    table.appendChild(thead);
-    table.appendChild(tbody);
-
-    resultDiv.appendChild(table);
-
-    updateTable(resultDiv.innerHTML);
+    createTable(theadInnerHtml, rowInnerHtml);
   }
 
   ).catch(err =>
@@ -273,4 +270,112 @@ function getTime(distance) {
   const formattedSeconds = String(seconds).padStart(2, '0');
 
   return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+}
+
+async function getRaidBounties(){
+  const farmListId = await getProfile();
+
+  fetch("https://ts8.x1.europe.travian.com/api/v1/graphql", {
+    "headers": {
+      "accept": "application/json, text/javascript, */*; q=0.01",
+      "accept-language": "en-US,en;q=0.9,tr;q=0.8",
+      "content-type": "application/json; charset=UTF-8",
+      "priority": "u=1, i",
+      "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Brave\";v=\"128\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "sec-gpc": "1",
+      "x-requested-with": "XMLHttpRequest",
+      "x-version": "2546.3"
+    },
+    "referrer": "https://ts8.x1.europe.travian.com/build.php?id=39&gid=16&tt=99",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": `{\"query\":\"query($id: Int!, $onlyExpanded: Boolean){bootstrapData{timestamp}weekendWarrior{isNightTruce}farmList(id: $id){id name slotsAmount runningRaidsAmount isExpanded sortIndex lastStartedTime sortField sortDirection useShip ownerVillage{id troops{ownTroopsAtTown{units{t1 t2 t3 t4 t5 t6 t7 t8 t9 t10}}}}defaultTroop{t1 t2 t3 t4 t5 t6 t7 t8 t9 t10}slotStates: slots{id isActive}slots(onlyExpanded: $onlyExpanded){id target{id mapId x y name type population}troop{t1 t2 t3 t4 t5 t6 t7 t8 t9 t10}distance isActive isRunning runningAttacks nextAttackAt lastRaid{reportId authKey time booty{resourceType{id code}amount}bootyMax icon}totalBooty{booty raids}}}}\",\"variables\":{\"id\":${farmListId},\"onlyExpanded\":false}}`,
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "include"
+  }).then( res => res.json())
+    .then( response => {
+      const sources = [0, 0, 0, 0];
+
+      response.data.farmList.slots.forEach( slot => {
+        sources.forEach( (source, i) => 
+          sources[i] = source + slot.lastRaid.booty[i].amount
+        )
+      });
+
+      const theadInnerHtml = `
+          <tr>
+            <th>Odun</th>
+            <th>Tuğla</th>
+            <th>Demir</th>
+            <th>Tahıl</th>
+          </tr>
+        `;
+
+      const rowInnerHtml = [`
+            <td>${sources[0]}</td>
+            <td>${sources[1]}</td>
+            <td>${sources[2]}</td>
+            <td>${sources[3]}</td>
+          `];
+
+      createTable(theadInnerHtml, rowInnerHtml);
+    });
+}
+
+function createTable(theadInnerHtml, rowInnerHtml) {
+  const resultDiv = document.createElement("div");
+  const table     = document.createElement("table");
+  const thead     = document.createElement("thead");
+  const tbody     = document.createElement("tbody");
+
+  thead.innerHTML = theadInnerHtml;
+
+  rowInnerHtml.forEach( (row) => {
+    const trow = document.createElement("tr");
+    
+    trow.innerHTML = row;
+    tbody.appendChild(trow);
+  });
+
+  table.appendChild(thead);
+  table.appendChild(tbody);
+
+  resultDiv.appendChild(table);
+
+  updateTable(resultDiv.innerHTML);
+}
+
+async function getProfile() {
+  const response = await fetch("https://ts8.x1.europe.travian.com/api/v1/graphql", {
+    "headers": {
+      "accept": "application/json, text/javascript, */*; q=0.01",
+      "accept-language": "en-US,en;q=0.9,tr;q=0.8",
+      "content-type": "application/json; charset=UTF-8",
+      "priority": "u=1, i",
+      "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Brave\";v=\"128\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "sec-gpc": "1",
+      "x-requested-with": "XMLHttpRequest",
+      "x-version": "2546.3"
+    },
+    "referrer": "https://ts8.x1.europe.travian.com/build.php?id=39&gid=16",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": "{\"query\":\"query($onlyExpanded: Boolean){bootstrapData{timestamp}weekendWarrior{isNightTruce}ownPlayer{isSitter isInVacationMode beginnersProtection accessRights{sendRaids}banInfo{type}village{id tribeId}villages{id name hasRallyPoint hasHarbour tribeId}abandonedFarmLists{id name slotsAmount runningRaidsAmount isExpanded sortIndex lastStartedTime sortField sortDirection useShip ownerVillage{id}defaultTroop{t1 t2 t3 t4 t5 t6 t7 t8 t9 t10}slotStates: slots{id isActive}slots(onlyExpanded: $onlyExpanded){id target{id mapId x y name type population}troop{t1 t2 t3 t4 t5 t6 t7 t8 t9 t10}distance isActive isRunning runningAttacks nextAttackAt lastRaid{reportId authKey time booty{resourceType{id code}amount}bootyMax icon}totalBooty{booty raids}}}farmLists{id name slotsAmount runningRaidsAmount isExpanded sortIndex lastStartedTime sortField sortDirection useShip ownerVillage{id troops{ownTroopsAtTown{units{t1 t2 t3 t4 t5 t6 t7 t8 t9 t10}}}}defaultTroop{t1 t2 t3 t4 t5 t6 t7 t8 t9 t10}slotStates: slots{id isActive}slots(onlyExpanded: $onlyExpanded){id target{id mapId x y name type population}troop{t1 t2 t3 t4 t5 t6 t7 t8 t9 t10}distance isActive isRunning runningAttacks nextAttackAt lastRaid{reportId authKey time booty{resourceType{id code}amount}bootyMax icon}totalBooty{booty raids}}}deactivatedFarmListTargets{id mapId x y name type}}}\",\"variables\":{\"onlyExpanded\":true}}",
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "include"
+  })
+
+  const data = await response.json();
+
+  return data.data.ownPlayer.farmLists[0].id;
 }
