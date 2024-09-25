1eba65676cca9c5547561414ead00d63ec8e4505
Can Gür <49841094+cangurdev@users.noreply.github.com>
2024-09-25 23:49:59 +0300

    add coordinates save method

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggleSwitch = document.getElementById('toggle-switch');
     
-  chrome.storage.local.get(['toggleEnabled'], function(result) {
+  chrome.storage.local.get(['toggleEnabled', 'xCoordinate', 'yCoordinate'], function(result) {
       if (result.toggleEnabled !== undefined) {
           toggleSwitch.checked = result.toggleEnabled;
       }
@@ -10,8 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
           chrome.tabs.sendMessage(tabs[0].id, { action: "load" });
         });
       }
-  });
 
+      document.getElementById('x-coordinate').value = result.xCoordinate || '';
+      document.getElementById('y-coordinate').value = result.yCoordinate || '';
+  });
  toggleSwitch.addEventListener('change', function(event) {    
     const isEnabled = event.target.checked;
     
@@ -19,4 +21,16 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Toggle state saved:', isEnabled);
     });
   });
+
+  document.getElementById('save-button').addEventListener('click', () => {
+    const xCoordinate = document.getElementById('x-coordinate').value;
+    const yCoordinate = document.getElementById('y-coordinate').value;
+    
+    chrome.storage.local.set({
+      xCoordinate: xCoordinate,
+      yCoordinate: yCoordinate
+    }, () => {
+      console.log('Settings saved');
+    });
+    });
 });
